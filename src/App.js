2ef80a31eb756aa273ef3282656f54c3b814f576
Antonela Gorobisvshi <gorobivschiantonela59@gmail.com>
2023-11-03 12:58:26 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,40 @@
+function App() {
+ 
+return ( 
+  <div>
+  <header>
+      <title>STEAM on WHEELS</title>
+  </header>
+  <body>
+      <header>
+          <img class="logo" src="./icon.jpg"></img>
+         <navbar>
+            <a href="index.html">Home</a>
+            <a href="pages/about.html">About</a>
+            <a href="pages/explore.html">Explore</a>
+            <a href="pages/contacts.html">Contacts</a>
+         </navbar> 
+      </header>
+  
+    <main>
+       <section id="index">
+          <div class="index-info">
+              <h1>STEAM on Wheels</h1>
+              <br></br>
+              <a href="" class="link-index">Afla mai mult</a>
+          </div>
+         
+       </section>
+    </main>
+  <footer>
+      <span>Tekedu</span>
+  </footer>
+<br></br>
+  </body>
+  </div>
+  )
+   
+
+}
+
+export default App;
